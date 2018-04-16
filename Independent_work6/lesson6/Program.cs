b86dd897724681f5b58b15a5fbc6f1a3cdd2645a
Unknown <v.a.mishchenko91@gmail.com>
2018-04-16 19:02:47 +0300

    IW6
    Даны числа А и B. А < B. Выведите сумму всех чисел расположенных между данными числами на экран.

---
+++ Independent_work6/lesson6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            int resualt = 0;
+
+            for (++x; x < y; x++)
+            {
+                resualt += x;
+            }
+
+            Console.WriteLine("Сумма чисел равна {0}", resualt);
+            Console.ReadKey();
+        }
+    }
+}
