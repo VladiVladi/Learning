937b23279c3cf9d765589b187be09df61e4de0c9
Unknown <v.a.mishchenko91@gmail.com>
2018-04-16 15:33:46 +0300

    HW3
    Нужно с консоли получить число (Кол-во $) и перевести их в рубли

---
+++ HomeWork3/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 56.9, b = 0;
+             
+            Console.WriteLine("Введите количество долларов");
+            string c = Console.ReadLine();
+            int d = Convert.ToInt32(c);
+            Console.WriteLine("{0} долларов равно {1} рублей", d, b = a * d);
+
+            Console.ReadKey();
+        }
+    }
+}
