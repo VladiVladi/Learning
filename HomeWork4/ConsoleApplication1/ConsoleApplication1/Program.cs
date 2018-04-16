49d2fbe9e965a7a2a2acb8746d805e7e92008017
Unknown <v.a.mishchenko91@gmail.com>
2018-04-16 17:57:40 +0300

    HW4
    Нужно с консоли получить число (Количество долларов) и перевести их в рубли.
    Проверить не является ли число отрицательным.
    Результат на экран.

---
+++ HomeWork4/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "HomeWork4";
+
+            decimal a = 56.9M, b = 0;
+
+            Console.WriteLine("Введите количество долларов");
+            string c = Console.ReadLine();
+            decimal d = Convert.ToDecimal(c);
+
+            if (d < 0)
+            {
+                Console.WriteLine("Вы ввели отрицательное число, пожалуйста, введите заново");
+            }
+            else
+            {
+                Console.WriteLine("За {0} долларов Вы получите {1} рублей", d, b = (a * d));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
