52d9f97bdcf31c8c00b881b4e3b5297e7862319b
Unknown <v.a.mishchenko91@gmail.com>
2018-04-16 17:36:28 +0300

    HW2
    Вывести заданный текст на консоль

---
+++ HomeWork2/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "HomeWork2";
+            
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("=================================================");
+            Console.ResetColor();
+
+            Console.WriteLine("\tПривет, я долларовый калькулятор!");
+            Console.Write("\tНа сегодня курс доллара");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(" 56.9 руб.\n");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("=================================================");
+            Console.ResetColor();
+
+
+            Console.ReadKey();
+        }
+    }
+}
