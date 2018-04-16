811ea542bd08f6b903e6970df7fc79a7118f4f44
Unknown <v.a.mishchenko91@gmail.com>
2018-04-16 18:06:03 +0300

    HW5
    Нужно с консоли получить число (Количество долларов) и перевести их в рубли.
    Проверить не является ли число отрицательным.
    Округлить результат перевода до целых (нет дробной части)
    Результат на экран.

---
+++ HomeWork5/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 56.9, b = 0;
+
+            Console.WriteLine("Введите количество долларов");
+            string c = Console.ReadLine();
+            double d = Convert.ToDouble(c);
+
+            if (d < 0)
+            {
+                Console.WriteLine("Вы ввели отрицательное число, пожалуйста, введите заново");
+            }
+            else
+            {
+                Console.WriteLine("За {0} долларов Вы получите {1} рублей", d, b = a * d);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
