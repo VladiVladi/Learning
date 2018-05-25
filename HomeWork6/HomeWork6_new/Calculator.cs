19364153396e89bd4b0c54f674280fab71c97db7
Unknown <v.a.mishchenko91@gmail.com>
2018-05-25 18:03:52 +0300

    HomeWork6

---
+++ HomeWork6/HomeWork6_new/Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork6_new
+{
+    class Calculator
+    {
+        double summ;
+        int temp;
+
+        public Calculator(double summ)
+        {
+            this.summ = summ;
+        }
+
+        public void Cash()
+        {
+            double[] money = new double[] { 5000, 2000, 1000, 500, 200, 100, 50, 10, 5, 2, 1 };
+
+            for (int i = 0; i < money.Length; i++)
+            {
+                if (summ >= money[i])
+                {
+                    temp = (int)(summ / money[i]);
+                    summ = summ - (money[i] * temp);
+                    Console.WriteLine("Вам будут выданы купюры/монеты номиналом {0} рублей в количестве {1} штук", money[i], temp);
+                }
+                else if (summ == 0)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
+
+
---
+++ HomeWork6/HomeWork6_new/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork6_new
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "HomeWork6";
+
+        Label1:
+            Console.WriteLine("Введите количество долларов:");
+            double dollars = Convert.ToDouble(Console.ReadLine());
+            int summ;
+
+            if (dollars <= 0)
+            {
+                Console.WriteLine("Не корректно введенное число. \nПожалуйста, повторите ввод!");
+                goto Label1;
+            }
+
+            else
+            {
+                summ = (int)(56.9 * dollars);
+                Console.WriteLine("Сумма для выдачи: {0} рублей", summ);
+            }
+
+            Calculator instance = new Calculator(summ);
+            instance.Cash();
+
+            Console.ReadKey();
+        }
+    }
+}
